e95927c6fd3625e10d08d8a81ef567f721cbf143
Jesus Violante <jv1108434@student.francistuttle.edu>
2024-02-13 08:13:48 -0600

    _--

--- src/App.js
+++
@@ -1,6 +0,0 @@
-export default function App() {
-  <>
-    <div></div>
-    <div></div>
-  </>;
-}
---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Tooth Brush", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+const Logo = () => {
+  return <h1>Far Away</h1>;
+};
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      {/* The code below creates an array from 1 -20 and maps over them while creating 20 <option> values with a number inside of them */}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/*(Regarding the abovve line) Casting the value to a number because target.value is always a string(Here we want a number) */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      {/* Maps over the set of initial items and displays them */}
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      {/*If the packed value for an item is true, it strikes through that item  */}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {/*Displays the items quantity and description. */}
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>You have X items in your list, and you have packed (x%)</em>
+    </footer>
+  );
+};
